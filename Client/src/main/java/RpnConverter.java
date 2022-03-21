17a44bb8254b589141a98ff78b02de38cb0dffbb
Zhenya Barsamov <hidan.99.99@mail.ru>
2022-03-21 19:35:27 +0500

    Дополнено описание классов клиентского приложения

--- Client/src/main/java/RpnConverter.java
+++ Client/src/main/java/RpnConverter.java
@@ -4,8 +4,11 @@ import java.util.Stack;
 // в обратную польскую запись в виде стека токенов выражения.
 // Для получения токенов выражения используется класс LexicalAnalyzer.
 public class RpnConverter {
+    private LexicalAnalyzer lexer;
+
     // Преобразовать выражение в обратную польскую запись и вернуть её в виде стека токенов выражения
     public Stack<Token> getExpressionRpn(String expression) throws Exception {
+        lexer = new LexicalAnalyzer(expression);
         throw new Exception("Не реализовано");
     }
 }
