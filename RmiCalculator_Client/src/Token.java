8285ea0b750b2108d2ca843554acc19f3cd0c2b0
Zhenya Barsamov <hidan.99.99@mail.ru>
2022-03-21 18:42:05 +0500

    Промежуточный коммит со структурой клиентского приложения

---
+++ RmiCalculator_Client/src/Token.java
@@ -0,0 +1,13 @@
+public class Token {
+    private boolean isOperation;
+    public boolean isOperation() {
+        return isOperation;
+    }
+
+    private char operation;
+    public char getOperation() {
+        return operation;
+    }
+
+    
+}
