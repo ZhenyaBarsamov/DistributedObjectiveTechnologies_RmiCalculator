8e2d04c22e6095fd5bccb2eac14fad5ac7fdb3f5
jurass <zapolnen@gmail.com>
2022-03-29 20:53:37 +0500

    RpnConverter

--- Client/src/main/java/RpnConverter.java
+++ Client/src/main/java/RpnConverter.java
@@ -5,10 +5,78 @@ import java.util.Stack;
 // Для получения токенов выражения используется класс LexicalAnalyzer.
 public class RpnConverter {
     private LexicalAnalyzer lexer;
+    
+    static int Prec(char ch)
+    {
+        switch (ch)
+        {
+        case '+':
+        case '-':
+            return 1;
+      
+        case '*':
+        case '/':
+            return 2;
+      
+        case '^':
+            return 3;
+        }
+        return -1;
+    }
+      
+    static String infixToPostfix(String exp)
+    {
+        String result = new String("");
+         
+        Stack<Character> stack = new Stack<>();
+         
+        for (int i = 0; i<exp.length(); ++i)
+        {
+            char c = exp.charAt(i);
+             
+
+            if (Character.isDigit(c)||c=='.')
+                result += c;
+              
+
+            else if (c == '(')
+                stack.push(c);
+             
+
+            else if (c == ')')
+            {
+                while (!stack.isEmpty() &&
+                        stack.peek() != '(')
+                    result +=  " "+stack.pop();
+                 
+                    stack.pop();
+            }
+            else 
+            {
+                while (!stack.isEmpty() && Prec(c)
+                         <= Prec(stack.peek())){
+                   
+                    result += " "+stack.pop();
+             }
+             if(!result.isEmpty())
+             result +=' ';
+                stack.push(c);
+            }
+      
+        }
+      
+        while (!stack.isEmpty()){
+            if(stack.peek() == '(')
+                return "Invalid Expression";
+            result += " "+stack.pop();
+         }
+        return result;
+    }
 
     // Преобразовать выражение в обратную польскую запись и вернуть её в виде стека токенов выражения
     public Stack<Token> getExpressionRpn(String expression) throws Exception {
-        lexer = new LexicalAnalyzer(expression);
-        throw new Exception("Не реализовано");
+	String resExp = infixToPostfix(expression);
+        lexer = new LexicalAnalyzer(resExp);
+        return lexer.allTokens()
     }
 }
