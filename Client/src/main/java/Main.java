2f386682bcf7ec68d3e0372527e0a4e5e680e846
Koffe-rgb <niccolum129@gmail.com>
2022-03-30 19:00:41 +0500

    try-catch добавлен

--- Client/src/main/java/Main.java
+++ Client/src/main/java/Main.java
@@ -1,6 +1,16 @@
+import java.rmi.NotBoundException;
+import java.rmi.RemoteException;
+
 public class Main {
-    public static void main(String[] args) throws Exception {
-        Controller controller = new Controller();
-        controller.readExpressionAndCalculate();
+    public static void main(String[] args) {
+        try {
+            Controller controller = new Controller();
+            controller.readExpressionAndCalculate();
+        } catch (NotBoundException | RemoteException e) {
+            System.out.println("Не удалось подключиться к серверу");
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+
     }
 }
