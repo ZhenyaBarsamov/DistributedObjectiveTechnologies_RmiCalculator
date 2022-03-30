4aea38f7896219d3275c0b416ec70d4be8a50345
Koffe-rgb <niccolum129@gmail.com>
2022-03-30 18:08:49 +0500

    Main

--- Client/src/main/java/Main.java
+++ Client/src/main/java/Main.java
@@ -1,6 +1,6 @@
-
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Привет, мир!");
+    public static void main(String[] args) throws Exception {
+        Controller controller = new Controller();
+        controller.readExpressionAndCalculate();
     }
 }
