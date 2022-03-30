6ab1669d89b63bf3e8d31d889a1a8e9de5e31daa
Koffe-rgb <niccolum129@gmail.com>
2022-03-30 18:08:35 +0500

    готов Контроллер

--- Client/src/main/java/Controller.java
+++ Client/src/main/java/Controller.java
@@ -1,26 +1,86 @@
-import java.util.Stack;
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.Scanner;
 
-// Класс Контроллера - основного класса клиентского приложения
 public class Controller {
-    // Введённое выражение в виде строки
-    private String expression;
-    // Разобранное выражение в ОПЗ в виде стека токенов
-    private Stack<Token> expressionStack;
+    private final Validator validator;
+    private final RpnConverter converter;
+    private final Deque<Double> operands;
 
-    private Validator validator;
-    private RpnConverter converter;
-
-    // Считывание и вычисление выражения.
-    // Здесь выражение считывается из консоли, затем проверяется Валидатором,
-    // затем преобразуется в стек ОПЗ Преобразователем, и вычисляется с помощью сервисов.
-    // Вычисление по стеку, скорее всего, уйдёт в дополнительный приватный метод
-    public void readExpressionAndCalculate() throws Exception {
-        throw new Exception("Не реализовано");
-    }
-
-    // Конструктор
     public Controller() {
         validator = new Validator();
         converter = new RpnConverter();
+        operands = new ArrayDeque<>();
+    }
+    
+    public void readExpressionAndCalculate() throws Exception {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Для завершения работы введите 0");
+        String expression;
+
+        do {
+            System.out.print("Введите выражение: ");
+            expression = scanner.nextLine();
+
+            while ( !validator.checkValidity(expression) ) {
+                System.out.println("Некорректное выражение");
+                System.out.print("Введите выражение: ");
+                expression = scanner.nextLine();
+            }
+
+            if ( !"0".equals(expression) ) {
+                // TODO: вызывать метод evaluate
+                double result = 21;
+                System.out.println("Результат: " + result);
+            }
+
+        }  while ( !"0".equals(expression) );
+
+        System.out.println("Завершаем работу");
+    }
+
+    private double evaluate(String expression) throws Exception {
+        Deque<Token> postfixExpression = converter.getExpressionRpn(expression);
+
+        while ( !postfixExpression.isEmpty() ) {
+            Token token = postfixExpression.poll();
+
+            if ( !token.isOperation() ) {
+                operands.push( token.getOperand() );
+            }
+            else {
+                executeOperator( token.getOperation() );
+            }
+        }
+
+        double result = operands.pop();
+        if ( !operands.isEmpty() )
+            throw new Exception("Ошибка при вычислении");
+
+        return result;
+    }
+
+    private void executeOperator(char operator) {
+        double b = operands.pop();
+        double a = operands.pop();
+        // TODO: заменить несуществующие методы на вызов сервисов
+        switch (operator) {
+            case '+': {
+                operands.push(plus(a, b));
+                break;
+            }
+            case '-': {
+                operands.push(minus(a, b));
+                break;
+            }
+            case '*': {
+                operands.push(multiply(a, b));
+                break;
+            }
+            case '/': {
+                operands.push(divide(a, b));
+                break;
+            }
+        }
     }
 }
